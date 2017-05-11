8f4c4ad46bc6076b5753a95914d733c99301b3db
Dima Bart <dima.bart01@gmail.com>
2017-05-11 09:09:45 -0400

    Initial Commit

---
+++ Mortar/Mortar.h
@@ -0,0 +1,19 @@
+//
+//  Mortar.h
+//  Mortar
+//
+//  Created by Dima Bart on 2017-05-11.
+//  Copyright © 2017 Dima Bart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Mortar.
+FOUNDATION_EXPORT double MortarVersionNumber;
+
+//! Project version string for Mortar.
+FOUNDATION_EXPORT const unsigned char MortarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mortar/PublicHeader.h>
+
+
