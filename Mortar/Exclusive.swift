da804a79d024e8f12852087b36be64e978d27aea
Dima Bart <dima.bart01@gmail.com>
2017-05-14 10:02:07 -0400

    Update <-> to use multiplication precedence.

--- Mortar/Exclusive.swift
+++ Mortar/Exclusive.swift
@@ -11,7 +11,7 @@ import Foundation
 // ----------------------------------
 //  MARK: - Operator -
 //
-infix operator <->: AdditionPrecedence
+infix operator <->: MultiplicationPrecedence
 
 // ----------------------------------
 //  MARK: - Overloads -
